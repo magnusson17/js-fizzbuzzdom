2400f3a435a40229025e892f9304db9efb30c137
Ga <gabri.magni.97@gmail.com>
2022-03-11 14:26:49 +0100

    colors adjustment

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,12 +3,12 @@ let containerOne = document.getElementById("container-one");
 for (let i = 1; i < 101; i++) {
     
     if (! (i % 3 === 0) && ! (i % 5 === 0)) {
-        containerOne.innerHTML += `<div class="bg_blue square">${[i]}</div>`;
+        containerOne.innerHTML += `<div class="bg_1389B2 square">${[i]}</div>`;
     } else if (i % 3 === 0 && i % 5 === 0) {
-        containerOne.innerHTML += `<div class="bg_purple square">fizzbuzz</div>`;
+        containerOne.innerHTML += `<div class="bg_F0466F square">fizzbuzz</div>`;
     } else if (i % 3 === 0) {
-        containerOne.innerHTML += `<div class="bg_aqua square">fizz</div>`;
+        containerOne.innerHTML += `<div class="bg_0BD6A1 square">fizz</div>`;
     } else if (i % 5 === 0) {
-        containerOne.innerHTML += `<div class="bg_orange square">buzz</div>`;
+        containerOne.innerHTML += `<div class="bg_FFD166 square">buzz</div>`;
     }
 }
