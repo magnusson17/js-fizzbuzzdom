c114773267559f1f913e219ef5824af5004d2b4c
Ga <gabri.magni.97@gmail.com>
2022-03-11 14:21:55 +0100

    style draft

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,12 +3,12 @@ let containerOne = document.getElementById("container-one");
 for (let i = 1; i < 101; i++) {
     
     if (! (i % 3 === 0) && ! (i % 5 === 0)) {
-        containerOne.innerHTML += `<div class="bg_blue">${[i]}</div>`;
+        containerOne.innerHTML += `<div class="bg_blue square">${[i]}</div>`;
     } else if (i % 3 === 0 && i % 5 === 0) {
-        containerOne.innerHTML += `<div class="bg_purple">fizzbuzz</div>`;
+        containerOne.innerHTML += `<div class="bg_purple square">fizzbuzz</div>`;
     } else if (i % 3 === 0) {
-        containerOne.innerHTML += `<div class="bg_aqua">fizz</div>`;
+        containerOne.innerHTML += `<div class="bg_aqua square">fizz</div>`;
     } else if (i % 5 === 0) {
-        containerOne.innerHTML += `<div class="bg_orange">buzz</div>`;
+        containerOne.innerHTML += `<div class="bg_orange square">buzz</div>`;
     }
 }
